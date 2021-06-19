fd79e93f4a187613abd17dbf0933525d3c236ec2
hisho <hisho.web@gmail.com>
2021-06-20 05:18:58 +0900

    ✨ import Row compontns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import { VFC } from 'react';
 import { Layout, Head } from '@src/layouts';
+import { Row } from '@src/components';
+import { requests } from '@src/configs';
 import { PageContext } from '@src/store';
 import { usePageReducer } from '@src/hooks';
 
 const Page: VFC = () => {
   const currentPage = usePageReducer('1');
-
   return (
     <PageContext.Provider value={currentPage}>
       <Layout>
         <Head />
-
+        <Row
+          title="NETFLIX ORIGUINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          large={true}
+        />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentMovies} />
       </Layout>
     </PageContext.Provider>
   );
