2589fb7c5211cb9a05dd92c511b9af8c0c3ade99
hisho <hisho.web@gmail.com>
2021-06-20 04:01:47 +0900

    ✨ add axios.ts

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,3 +1,4 @@
 export { percentage } from '@src/helper/percentage';
 export { rem } from '@src/helper/rem';
 export { em } from '@src/helper/em';
+export { axiosInstance } from '@src/helper/axios';
