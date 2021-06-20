5dde9ad4873875904d8f22f9d406a8ed8bf12e19
hisho <hisho.web@gmail.com>
2021-06-20 21:39:45 +0900

    ✨ add Spacer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { VFC } from 'react';
 import { Layout, Head } from '@src/layouts';
-import { Row } from '@src/components';
+import {Row, Spacer} from '@src/components';
 import { requests } from '@src/configs';
 import { PageContext } from '@src/store';
 import { usePageReducer } from '@src/hooks';
@@ -16,11 +16,17 @@ const Page: VFC = () => {
           fetchUrl={requests.fetchNetflixOriginals}
           large={true}
         />
+        <Spacer className="h-10 sm:h-20" />
         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Spacer className="h-10 sm:h-20" />
         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Spacer className="h-10 sm:h-20" />
         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Spacer className="h-10 sm:h-20" />
         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Spacer className="h-10 sm:h-20" />
         <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Spacer className="h-10 sm:h-20" />
         <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentMovies} />
       </Layout>
     </PageContext.Provider>
